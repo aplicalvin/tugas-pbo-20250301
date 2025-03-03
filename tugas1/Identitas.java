e5b852323682f4515beaf70241cb170c551b31f4
ApliCalvin <thecalvins.sims@gmail.com>
2025-03-03 14:29:48 +0700

    Tugas 1a - Program Tampil Identitas : - Dengan print identitas secara manual (Menggunakan System.Out.Println)
    by ApliCalvin on 03/03/2025 at 14:29

---
+++ tugas1/Identitas.java
@@ -0,0 +1,18 @@
+package tugas1;
+
+// PERCOBAAN 1 - Langsung di tampilkan melalui System.out.println
+public class Identitas{
+    public static void main(String[] args) {
+        System.out.println("NIM        : A11.2023.14880");
+        System.out.println("NAMA       : Calvin Samuel Simbolon");
+        System.out.println("Alamat     : Perum. Graha Mukti Residen No. 94");
+        System.out.println("Kota       : Kota Semarang");
+        System.out.println("Kode Pos   : 50195");
+        System.out.println("Telp.      : -");
+        System.out.println("Hp         : 081227426908");
+        System.out.println("Email      : 111202314880@mhs.dinus.ac.id");
+
+    }
+}
+
+
